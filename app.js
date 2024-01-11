378ab105552e84106ea87ce192b2638e469a79c3
JSJ <jsj950611@naver.com>
2024-01-11 22:53:09 +0900

    네이버로그인api 브랜치 시작

--- app.js
+++ app.js
@@ -2,9 +2,6 @@ const express = require("express");
 const app = express();
 const routers = require('./router')
 
-
-
-
 app.use(express.static('public'))
 app.set("views", __dirname);
 app.set("view engine", "ejs");
